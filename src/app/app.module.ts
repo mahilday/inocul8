d042d21706263725bac8dadf5c137b64d4c0775b
mahilday <mathildaimadojiemu@gmail.com>
2020-08-16 22:38:12 +0100

    added validation, post method for the forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MyFormComponent} from "./form/myform.component"
+import {MyFormComponent} from "./form/myform.component";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyFormComponent
+    MyFormComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule, 
+    FormsModule, 
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/myform.component.ts
+++ src/app/form/myform.component.ts
@@ -1,53 +1,116 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForm, FormGroup } from '@angular/forms';
 
 @Component({
-    selector: 'my-form',
-    templateUrl: './myform.component.html',
-    styleUrls: ['./myform.component.css']
+  selector: 'my-form',
+  templateUrl: './myform.component.html',
+  styleUrls: ['./myform.component.css'],
 })
-export class MyFormComponent {
-    myself: boolean = false;
-    family: boolean = false;
-    corporate:boolean = false;
-    home:boolean = false;
-    hub:boolean = false;
+export class MyFormComponent implements OnInit {
+  myself: boolean = false;
+  family: boolean = false;
+  corporate: boolean = false;
+  home: boolean = false;
+  hub: boolean = false;
 
-    states = [
-        'Anambra state', 'Edo state', 'Abia State'
-    ];
-    vaccine: Array<string> =[
-        "Measles","Polio"
-    ]
-    lga: Array<string> =[
-        "Uhunwonde","Ikpoba-Hill"
-    ]
-    clickHome() {
-        this.home = true;
-        this.hub = false;
-    }
-    clickHub() {
-        this.home = false;
-        this.hub = true;
-    }
+  states = ['Anambra state', 'Edo state', 'Abia State'];
+  vaccine: Array<string> = ['Measles', 'Polio'];
+  lga: Array<string> = ['Uhunwonde', 'Ikpoba-Hill'];
 
-    clickMyself(){
-        this.myself = true;
-        this.family = false;
-        this.corporate = false
-    }
-    clickFamily() {
-        this.myself = false;
-        this.family = true;
-        this.corporate = false
-    }
-    clickCorporate() {
-        this.myself = false;
-        this.family = false;
-        this.corporate = true;
-    }
-    clickBack(){
-        this.myself= false;
-        this.family= false;
-        this.corporate = false;
-    }
+  clickHome() {
+    this.home = true;
+    this.hub = false;
+  }
+  clickHub() {
+    this.home = false;
+    this.hub = true;
+  }
+
+  clickMyself() {
+    this.myself = true;
+    this.family = false;
+    this.corporate = false;
+  }
+  clickFamily() {
+    this.myself = false;
+    this.family = true;
+    this.corporate = false;
+  }
+  clickCorporate() {
+    this.myself = false;
+    this.family = false;
+    this.corporate = true;
+  }
+  clickBack() {
+    this.myself = false;
+    this.family = false;
+    this.corporate = false;
+    console.log(this.profile)
+  }
+  ngOnInit(){
+
+  }
+  
+  profile:Array<object>=[]
+  formModel: any={}
+  famModel: any={
+    statefam:'',
+    lgafam:'',
+    preferredhubfam:'',
+    addressfam:'',
+    timefam:'',
+    profile: this.profile
+  }
+  corpModel: any={}
+  nopersons:number = this.profile.length
+  valid: boolean = false
+  saveProfile=(e, profile: object)=>{
+    e.preventDefault();
+    this.profile.push(profile)
+    this.nopersons += 1;
+  }
+  resetProfile=(e, profile:FormGroup)=>{
+    event.preventDefault()
+    profile.reset()
+  }
+  
+  constructor(private http: HttpClient) {}
+
+  postForm(val: NgForm) {
+    let url = 'http://localhost:3000/api/v1/corporate-form';
+    
+    this.http.post(url, val.value).toPromise().then((res: any)=>{
+        this.valid= true
+        console.log(res)
+    }).catch(err=>{
+        console.log(err)
+    })
+  }
+  postMyForm(val: NgForm) {
+    let url = 'http://localhost:3000/api/v1/my-form';
+
+    this.http.post(url, val.value).toPromise().then((res: any) => {
+      console.log(res)
+      if(res.status){
+        this.valid = true
+      } else{
+        this.valid = false
+      }
+     
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+  postFamForm() {
+    let url = 'http://localhost:3000/api/v1/family-form';
+
+    this.http.post(url, this.famModel).toPromise().then((res: any) => {
+      this.valid = true
+      console.log(res, this.valid)
+      
+    }).catch(err => {
+      console.log(err)
+    })
+  }
 }
