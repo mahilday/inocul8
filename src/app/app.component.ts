3172db879afdb5b47b5684cf787b75111158fa29
mahilday <mathildaimadojiemu@gmail.com>
2020-11-13 05:06:15 +0100

    google analytics and fbq

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ProfileService} from "./services/profile.service"
+import { Router, NavigationEnd } from '@angular/router';
+declare let gtag:Function;
+declare let fbq:Function;
 
 @Component({
   selector: 'app-root',
@@ -8,8 +11,13 @@ import {ProfileService} from "./services/profile.service"
 })
 export class AppComponent implements OnInit {
   title = 'pharmaceutical';
-  constructor(private profileService: ProfileService){
-
+  constructor(private profileService: ProfileService, private router: Router){
+    router.events.subscribe((y: NavigationEnd) => {
+      if(y instanceof NavigationEnd){
+        gtag('config','UA-{ID}',{'page_path' : y.url});
+        fbq('track', 'PageView');
+      }
+    })
   }
   ngOnInit(){
   }
